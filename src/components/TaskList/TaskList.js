a03752d56005966487919dd23925dc48c7e7151b
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-05-30 15:02:20 +0300

    test

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,35 @@
+import { Task } from "components/Task/Task";
+import { useSelector } from "react-redux";
+import { statusFilters } from "components/redux/constants";
+import css from "./TaskList.module.css";
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch(statusFilter) {
+    case statusFilters.active: 
+    return tasks.filter(task => !task.completed)
+    case statusFilters.completed:
+      return tasks.filter(task => task.comleted)
+      default:
+        return tasks;
+  }
+};
+
+
+export const TaskList = () => {
+  const tasks = useSelector(state => state.tasks);
+  const statusFilter = useSelector(state => state.filters.status);
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  return (
+    <ul 
+    className={css.list}
+    >
+      {visibleTasks.map(task => (
+        <li 
+        className={css.listItem}
+         key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
